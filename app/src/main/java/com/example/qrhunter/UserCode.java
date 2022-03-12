322a353359cf58c00a137a93112446ac6a8bb4f4
xiaoluzzz <zxl9487@gmail.com>
2022-03-12 14:37:38 -0700

    add Usercode

--- app/src/main/java/com/example/qrhunter/UserCode.java
+++ app/src/main/java/com/example/qrhunter/UserCode.java
@@ -1,4 +1,15 @@
 package com.example.qrhunter;
 
-public class UserCode {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usercode);
+    }
 }
