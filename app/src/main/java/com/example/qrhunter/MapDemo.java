9a0a898558ad73441f1445ab5f43e55b4cf0c3ee
Sihe Wan <sihe@ualberta.ca>
2022-03-22 23:54:37 -0600

    show my current location on the Map test

--- app/src/main/java/com/example/qrhunter/MapDemo.java
+++ app/src/main/java/com/example/qrhunter/MapDemo.java
@@ -3,9 +3,17 @@ package com.example.qrhunter;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,9 +28,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.qrhunter.databinding.ActivityMapDemoBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
@@ -32,6 +42,7 @@ public class MapDemo extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private ActivityMapDemoBinding binding;
     FirebaseFirestore db;
+    private int ACCESS_LOCATION_REQUEST_CODE = 10001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +60,40 @@ public class MapDemo extends FragmentActivity implements OnMapReadyCallback {
             }
         });
 
+
+        /*Button mylocation = findViewById(R.id.currentlocation);
+        mylocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+         */
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        SearchView searchView = (SearchView) findViewById(R.id.txtSearchCode);
+        searchView.setIconified(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //search location to find QRcodes nearby
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText != null && newText.length() > 0) {
+                    Log.d("", "text change");
+                }
+                return true;
+            }
+        });
     }
 
     /**
@@ -69,25 +110,45 @@ public class MapDemo extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(53.54237203764759, -113.49141013307299);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+//        LatLng sydney = new LatLng(53.54237203764759, -113.49141013307299);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            enableUserlocation();
+
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+            }
+        }
+
 
         //implement multiple markers on the map
         db = FirebaseFirestore.getInstance();
         CollectionReference locationRef = db.collection("QRCodes");
-        locationRef.get()
+        locationRef
+                .whereEqualTo("shareLocation", true)
+                .get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                         Log.d(TAG, "onSuccess: getting the data");
                         List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
-                        for (DocumentSnapshot snapshot: snapshotList) {
+                        for (DocumentSnapshot snapshot : snapshotList) {
                             //Log.d(TAG, "onSuccess: " + snapshot.getData().toString());
                             Log.d(TAG, "onSuccess: " + snapshot.getGeoPoint("geoPoint"));
-                            double longitude;
+                            GeoPoint geoPoint = snapshot.getGeoPoint("geoPoint");
+                            double lat = geoPoint.getLatitude();
+                            double lng = geoPoint.getLongitude();
+                            LatLng latLng = new LatLng(lat, lng);
+                            mMap.addMarker(new MarkerOptions().position(latLng).title(snapshot.getString("qrid")));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                            mMap.getUiSettings().setZoomControlsEnabled(true);
 
                         }
                     }
@@ -100,5 +161,33 @@ public class MapDemo extends FragmentActivity implements OnMapReadyCallback {
                 });
 
 
+    }
+
+    private void enableUserlocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+  
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == ACCESS_LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableUserlocation();
+            }else{
+
+            }
+        }
     }
 }
